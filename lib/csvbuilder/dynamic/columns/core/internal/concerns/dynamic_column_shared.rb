0e4d663a660622bb6d28b9a35e37b278898b09b0
Joel Azemar <joel.azemar@gmail.com>
2022-12-29 16:51:02 +0100

    Update dynamic_column_shared.rb

--- lib/csvbuilder/dynamic/columns/core/internal/concerns/dynamic_column_shared.rb
+++ lib/csvbuilder/dynamic/columns/core/internal/concerns/dynamic_column_shared.rb
@@ -21,10 +21,24 @@ module Csvbuilder
     #
     # header models
     #
+    # return the collection given by the context
+    # i.e: skills: ["Ruby", "Python", "Javascript"]
+    # header_models => ["Ruby", "Python", "Javascript"]
+    #
+    # @return [Array] Array of values
     def header_models
       Array(context.public_send(header_models_context_key))
     end
 
+    # The name of the dynamic_column
+    #
+    # i.e: With dynamic_column :skills
+    # => :skills
+    #
+    # i.e: With dynamic_column :skills, as: :programming_languages
+    # => :programming_languages
+    #
+    # @return [Symbol] Symbol
     def header_models_context_key
       options[:as] || column_name
     end
